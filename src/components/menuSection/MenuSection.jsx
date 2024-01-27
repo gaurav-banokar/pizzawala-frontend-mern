96233dbd80fb095953bc1a478b6f14755621bf08
Gaurav Banokar <122355566+gaurav-banokar@users.noreply.github.com>
2024-01-27 23:04:40 +0530

    Update MenuSection.jsx

--- src/components/menuSection/MenuSection.jsx
+++ src/components/menuSection/MenuSection.jsx
@@ -42,7 +42,7 @@ const MenuSection = () => {
   };
 
   const displayedItems =
-    finalValue && finalValue ? itemsBySearch.length !== 0 && itemsBySearch : itemsBySearch.length !== 0 ? itemsByCategory : [];
+    finalValue && finalValue ? itemsBySearch :  itemsByCategory;
 
   return (
     <>
