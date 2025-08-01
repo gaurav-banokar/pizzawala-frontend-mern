fb8c4cf7f17b9ee9ba74de8b2cdfaf8fd9ab8ef6
Gaurav Banokar <122355566+gaurav-banokar@users.noreply.github.com>
2025-08-01 15:18:38 +0530

    fix-bug MyProfile.jsx

--- src/components/profile/MyProfile.jsx
+++ src/components/profile/MyProfile.jsx
@@ -39,7 +39,7 @@ const MyProfile = ({ admin }) => {
   const { user } = useSelector(state => state.auth)
   const { message } = useSelector(state => state.upload)
 
-  const  { profilePhoto } = useSelector(state => state.profilePhoto && state.profilePhoto.url)
+  const  { profilePhoto } = useSelector(state => state.profilePhoto.url)
 
   const imageHandler = (e) => {
     const file = e.target.files[0];
@@ -70,7 +70,7 @@ const MyProfile = ({ admin }) => {
 
 
   useEffect(() => {
-    dispatch(getProfilePhoto(user && user._id))
+    dispatch(getProfilePhoto(user._id))
   }, [dispatch])
 
   useEffect(() => {
